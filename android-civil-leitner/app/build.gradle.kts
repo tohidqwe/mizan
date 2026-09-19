ebb1d78230f40480e1f9befcb858ee46cbe224a3
tohidqwe <tohid.lawyer@gmail.com>
2026-09-19 20:49:40 +0330

    Version Doctor Tohid course as 1.0.0 internal release

--- android-civil-leitner/app/build.gradle.kts
+++ android-civil-leitner/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.mizan.civilleitner"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 3
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
